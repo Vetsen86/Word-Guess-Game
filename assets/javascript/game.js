293808780f3481576e3e3ee37cd3b859e224d506
Erik Beckstrand <erik.beckstrand@gmail.com>
2019-08-27 15:17:06 -0600

    choose word and show blank spaces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,33 @@
-// First things first: I need to capture the user's keystroke and save it to a variable.
+// First, I need to create an array of strings that will represent my words. 10 should suffice.
+var words = ["halo", "persona", "pacman", "mario", "zelda", "metroid", "battlefield", "lives", "diablo", "monsters"];
+
+
+//Then I need to get a random word from the array.
+function getWord() {
+    var randomIndex = Math.floor(Math.random() * words.length);
+    var wordArray = words[randomIndex].split('');
+    return wordArray;
+}
+
+function getSpaces(word) {
+    var spaceArray = [];
+    word.forEach(function (element) {
+        spaceArray.push("_");
+    });
+    document.getElementById("spaces").innerHTML = spaceArray.join(" ");
+    return spaceArray;
+}
+
+var word = getWord();
+console.log(word);
+
+var spaces = getSpaces(word);
+console.log(spaces);
+
+
+// I need to capture the user's keystroke and save it to a variable.
+
+document.onkeyup = function(event) {
+    var keyPressed = event.key;
+    console.log(keyPressed);
+}
